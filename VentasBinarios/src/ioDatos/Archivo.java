f6c0a7e85ffac4a9908605c3d0f4e52cd505c091
alumno <alumno@aula1-15>
2019-02-05 18:39:39 +0100

    4

--- VentasBinarios/src/ioDatos/Archivo.java
+++ VentasBinarios/src/ioDatos/Archivo.java
@@ -7,6 +7,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -27,11 +28,12 @@ public class Archivo {
     private static String linea;
 
   
-    public static void leerFichero(){
+    public static ArrayList<Ventas> leerFichero(){
         
         File f = new File("ventas.dat");
         FileInputStream fi = null;
         DataInputStream salida = null;
+        ArrayList<Ventas> vVentas = new ArrayList();
        
         
         if (!f.exists()) {
@@ -47,11 +49,12 @@ public class Archivo {
             salida = new DataInputStream(fi);
           while(true){
             Ventas v = new Ventas(salida.readUTF(), salida.readInt(), salida.readInt(), salida.readDouble());
+            vVentas.add(v);
           }
         } catch (FileNotFoundException ex) {
             System.out.println("Error al leer el archivo");
         } catch (IOException ex) {
-            System.out.println("Error de lectura de fichero");
+            System.out.println("Fin de lectura");
         }finally{
             try {
                 salida.close();
@@ -59,9 +62,26 @@ public class Archivo {
                 System.out.println("Error al cerrar el programa");
             }
         }
+        return vVentas;
  
     }
     
+    
+    public static void añadirVentas(){
+        File f = new File("ventas.dat");
+        FileInputStream fi = null;
+        DataInputStream salida = null;
+        ArrayList<Ventas> vVentas = new ArrayList();
+       
+        
+        if (!f.exists()) {
+            try {
+                f.createNewFile();
+            } catch (IOException ex) {
+                System.out.println("Error al crear el archivo");
+            }
+        } 
+    }
     
     
 }
