535461782bb23f4e5423cd38824af41b21173f60
alumno <alumno@aula1-15>
2019-02-05 18:28:06 +0100

    5

--- VentasBinarios/src/ventasbinarios/VentasBinarios.java
+++ VentasBinarios/src/ventasbinarios/VentasBinarios.java
@@ -15,7 +15,7 @@ public class VentasBinarios {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ioDatos.Archivo.leer();
+        ioDatos.Archivo.leerFichero();
     }
     
 }
