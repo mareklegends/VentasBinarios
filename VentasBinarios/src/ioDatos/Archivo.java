441c1b6ff20f1e6a398969c2192d0e5ad1b59b34
alumno <alumno@aula1-15>
2019-02-05 17:44:34 +0100

    1

---
+++ VentasBinarios/src/ioDatos/Archivo.java
@@ -0,0 +1,25 @@
+package ioDatos;
+
+import java.io.DataInputStream;
+import java.io.File;
+import javax.imageio.stream.FileImageInputStream;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author alumno
+ */
+public class Archivo {
+    
+    File f = new File("ventas.dat");
+    FileImageInputStream fi=null;
+    DataInputStream di = null;
+    
+
+    
+}
